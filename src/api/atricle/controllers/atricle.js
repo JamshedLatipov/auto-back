b3168e73a355cb479775484446ac4ec6afd2163f
Jamshed Latipov <jamshedlatipov@gmail.com>
2024-10-24 18:46:37 +0500

    f

--- src/api/atricle/controllers/atricle.js
+++ src/api/atricle/controllers/atricle.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::atricle.atricle');
+module.exports = createCoreController('api::atricle.atricle', ({strapi}) => ({
+    async findOne(ctx) {
+        const { id: vin } = ctx.params;
+        const { query } = ctx;
+        if (!query.filters) query.filters = {}
+        query.filters.vin = { '$eq': vin }
+        const entity = await strapi.service('api::atricle.atricle').find(query);
+        const { results } = await this.sanitizeOutput(entity, ctx);
+    
+        return this.transformResponse(results[0]);
+      }
+}));
